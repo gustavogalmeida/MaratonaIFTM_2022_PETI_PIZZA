87c898fc12ccc1a732a5cbd8142f009a2f79eb33
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-07 15:30:55 -0300

    Exercício Soma de Fatoriais concluído!

--- SomaDeFatoriais/src/main/java/Main.java
+++ SomaDeFatoriais/src/main/java/Main.java
@@ -3,35 +3,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // LENDO TECLADO
+        // CRIANDO SCANNER
         Scanner scanner = new Scanner(System.in);
-        String valores = scanner.nextLine();
 
-        // SEPARANDO VALORES
-        String[] valoresArray = valores.split(" ");
-        int m = Integer.parseInt(valoresArray[0]);
-        int n = Integer.parseInt(valoresArray[1]);
-        long nfat=n, mfat=m;
+        while (scanner.hasNext()) { // ENQUANTO TIVER PROXIMA LINHA EXECUTA
+            // LENDO TECLADO E GUARDANDO EM STRING
+            String valores = scanner.nextLine();
 
-        // TRATANDO CASO SENA 0! = 1
-        if (n == 0){
-            nfat = 1;
-        }
-        if (m == 0){
-            mfat = 1;
-        }
+            // SEPARANDO VALORES
+            String[] valoresArray = valores.split(" ");
+            int m = Integer.parseInt(valoresArray[0]);
+            int n = Integer.parseInt(valoresArray[1]);
+            long nfat = n, mfat = m;
 
-        // REALIZANDO OPERAÇÕES DO FATORIAL
-        while (n>1){
-            nfat = nfat * (n-1);
-            n--;
-        }
-        while (m>1){
-            mfat = mfat * (m-1);
-            m--;
-        }
+            // TRATANDO CASO SENA 0! = 1
+            if (n == 0) {
+                nfat = 1;
+            }
+            if (m == 0) {
+                mfat = 1;
+            }
 
-        // IMPRIMINDO RESULTADO
-        System.out.println(mfat+nfat);
+            // REALIZANDO OPERAÇÕES DO FATORIAL
+            while (n > 1) {
+                nfat = nfat * (n - 1);
+                n--;
+            }
+            while (m > 1) {
+                mfat = mfat * (m - 1);
+                m--;
+            }
+
+            // IMPRIMINDO RESULTADO
+            System.out.println(mfat + nfat);
+        }
     }
 }
